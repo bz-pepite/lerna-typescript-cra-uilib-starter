7fc6f228b127345cf550dcd5d747c4c6fd82d56e
benoit zegels <b.zegels@pepite.be>
2020-04-14 11:33:14 +0200

    some test changes
    :w
    :

--- packages/my-monorepo-cra-app/src/App.tsx
+++ packages/my-monorepo-cra-app/src/App.tsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import {MyNewComponent} from '@my-org/my-monorepo-ui-lib';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { MyNewComponent } from "@my-org/my-monorepo-ui-lib";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <MyNewComponent text="this is our library component"/>
+        <MyNewComponent text="this is our library component 2" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
--- packages/my-monorepo-ui-lib/src/components/mynewcomponent/index.tsx
+++ packages/my-monorepo-ui-lib/src/components/mynewcomponent/index.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 export interface MyNewComponentProps {
-	text: string;
+  text: string;
 }
 
 export const MyNewComponent = (props: MyNewComponentProps) => {
-	return (<span>{props.text}</span>)
-}
+  return <span>{props.text} That is a 12 modification inside the lib</span>;
+};
 
 export default MyNewComponent;
